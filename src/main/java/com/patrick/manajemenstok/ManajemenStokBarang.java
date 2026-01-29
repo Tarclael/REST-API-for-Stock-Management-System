79fc8137b4bba4dd7bd41f2ebcbc9f5080e6aed7
Patrick Kocu <tarclael5@gmail.com>
2026-01-30 06:45:03 +0700

    initial commit

---
+++ src/main/java/com/patrick/manajemenstok/ManajemenStokBarang.java
@@ -0,0 +1,13 @@
+package com.patrick.manajemenstok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManajemenStokBarang {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManajemenStokBarang.class, args);
+	}
+
+}
---
+++ src/test/java/com/patrick/manajemenstok/ManajemenStokBarangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrick.manajemenstok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManajemenStokBarangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
